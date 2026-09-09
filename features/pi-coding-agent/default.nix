df9451dcf5368856d0a4dce1c97a30df87751101
Deric Cheung <cheung.deric@gmail.com>
2026-09-08 22:05:58 -0700

    Add my pi-devcontainer package to pi. Remove unused pi packages

--- features/pi-coding-agent/default.nix
+++ features/pi-coding-agent/default.nix
@@ -23,25 +23,11 @@ in
           --registry ${lib.escapeShellArg npmRegistry} \
           "$@"
       '';
-
-      piToolDisplayConfig = (pkgs.formats.json { }).generate "config.json" {
-        registerToolOverrides = {
-          "write" = false;
-          "read" = false;
-          "grep" = false;
-          "edit" = false;
-          "bash" = false;
-          "find" = true;
-          "ls" = true;
-        };
-      };
     in
     {
       home.activation.ensurePiNpmCacheDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         mkdir -p ${lib.escapeShellArg piNpmCacheDir}
       '';
-
-      home.file."${piConfigDir}/extensions/pi-tool-display/config.json".source = piToolDisplayConfig;
 
       programs.pi-coding-agent = {
         enable = true;
@@ -60,37 +46,32 @@ in
           defaultModel = "claude-opus-5";
           defaultThinkingLevel = "high";
           packages = [
-            # Sandbox
-            "npm:pi-landstrip"
-            # Others
-            "npm:pi-mcp-adapter"
+            "git:github.com/Icohedron/pi-devcontainer"
             "npm:pi-simplify"
-            "npm:pi-tool-display"
             "npm:pi-zentui"
             "npm:pi-drawio"
-            "npm:pi-readseek"
             "npm:pi-smart-compact"
           ];
 
-          readseek = {
-            overrideTools = [
-              "write"
+          devcontainer = {
+            enabled = true;
+            runtime = "podman";
+            runtimeArgs = [ ];
+            execArgs = [ ];
+            devcontainerPath = "devcontainer";
+            upArgs = [ ];
+            hostCommands = [ "tuicr" ];
+            tools = [
               "read"
-              "grep"
+              "write"
               "edit"
+              "bash"
+              "grep"
+              "find"
+              "ls"
             ];
-            imageMode = "auto";
-            syntaxValidation = "warn";
-            timeoutMs = 120000;
-            grep = {
-              maxLines = 2000;
-              maxBytes = 51200;
-            };
-            display = {
-              grep = "compact";
-              edit = "expanded";
-              write = "expanded";
-            };
+            userBash = "container";
+            requireContainer = false;
           };
         };
       };
