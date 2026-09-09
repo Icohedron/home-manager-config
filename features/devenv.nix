0ca8da6863ebfedfe141619c7c1d039ea467e8cc
Deric Cheung <cheung.deric@gmail.com>
2026-09-08 22:06:36 -0700

    Fix secretspec. Disable devenv shell integration

--- features/devenv.nix
+++ features/devenv.nix
@@ -1,14 +1,22 @@
+# devenv - reproducible per-project developer environments.
 { config, ... }:
 {
   flake.modules.homeManager.devenv = {
     programs.devenv = {
-      enable = true;
-      enableBashIntegration = true;
-      enableNushellIntegration = true;
-      enableZshIntegration = true;
+      enable = true; # note: secretspec is also included with devenv
+      # Don't use devenv shell hooks due to: https://github.com/cachix/devenv/issues/3041
+      # Opt for direnv instead.
+      enableBashIntegration = false;
+      enableFishIntegration = false;
+      enableNushellIntegration = false;
+      enableZshIntegration = false;
+
+      # In .envrc:
+      # #!/usr/bin/env bash
+      # eval "$(devenv direnvrc)"
+      # use devenv
     };
   };
 
   flake.modules.homeManager.workstation.imports = [ config.flake.modules.homeManager.devenv ];
 }
-
